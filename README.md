49c01e6c3ca93ffb45c235a739cb1bc516403b37
linux <matan@garantiadata.com>
2014-08-18 16:24:03 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Memcached Cloud Ruby Sinatra Sample
+
+A Ruby Sinatra sample app that demonstrates [Memcached](http://memcached.org/) deployment to Heroku.<br />
+SET and GET a key, and get your db STATS.
+
+The app is currently running at: [memcachedcloud-ruby-sinatra-sample.herokuapp.com](http://memcachedcloud-ruby-sinatra-sample.herokuapp.com), and uses [Memcached Cloud](https://addons.heroku.com/memcachedcloud) as a backend.
+
+## Deploy to Heroku
+
+Instantly deploy it to Heroku:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/RedisLabs/memcachedcloud-ruby-sinatra-sample)
---
+++ app.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'dalli'
+
+configure do
+    if ENV["MEMCACHEDCLOUD_SERVERS"]
+        $cache = Dalli::Client.new(ENV["MEMCACHEDCLOUD_SERVERS"].split(','), :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"])
+    end
+end
+
+get '/' do
+  File.read(File.join('views', 'index.html'))
+end
+
+get '/command' do
+  unless $cache.nil?
+    @res= ''
+
+    begin
+      case params[:a]
+        when 'set'           
+          @res  = $cache.set('welcome_msg', 'Hello from Redis!').nil? ? 'False' : 'True'          
+        when 'get'
+          @res = $cache.get('welcome_msg') || 'N/A'
+        when 'delete'
+          @res = $cache.delete('welcome_msg').nil? ? 'N/A' : 'True'
+        when 'info'
+          $cache.stats().each { |k, v| 
+            @res += "#{k}: #{v}<br />" 
+          }
+      end
+    
+    rescue => e
+      puts e.message
+      @res = nil
+    end
+  
+    $cache.close()  
+  end
+
+  @res  
+end
